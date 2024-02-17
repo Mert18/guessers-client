b7a011aed030b41d175a743fcaa7e4559de10772
Mert Uyğur <mertuygur02@gmail.com>
2024-02-17 23:28:46 +0300

    4: generate user visual bug resolved

--- components/public/SixthStep.js
+++ components/public/SixthStep.js
@@ -3,9 +3,9 @@ import FadeInOut from "../common/FadeInOut";
 import { useRouter } from "next/navigation";
 
 const SixthStep = ({ createdUser }) => {
-   const router = useRouter();
-   const handleRedirectLogin = () => {
-     router.push("/main");
+  const router = useRouter();
+  const handleRedirectLogin = () => {
+    router.push("/main");
   };
 
   return (
@@ -13,14 +13,19 @@ const SixthStep = ({ createdUser }) => {
       <div className="flex flex-col justify-center items-center">
         <h1>You are created!</h1>
 
-        <div className="flex justify-center">
-          <p className="mr-2">Name:</p>
-          <p className="scale-125">{createdUser.name}</p>
-        </div>
-        <div className="flex justify-center">
-          <p className="mr-2">Identity Number:</p>
-          <p className="scale-125">{createdUser.identityNumber}</p>
-        </div>
+        {createdUser.name && createdUser.identityNumber && (
+          <>
+            <div className="flex justify-center">
+              <p className="mr-2">Name:</p>
+              <p className="scale-125">{createdUser.name}</p>
+            </div>
+            <div className="flex justify-center">
+              <p className="mr-2">Identity Number:</p>
+              <p className="scale-125">{createdUser.identityNumber}</p>
+            </div>
+          </>
+        )}
+
         <button
           className="border border-gray-400 rounded-md p-2 m-2 text-center hover:bg-gray-200"
           onClick={() => handleRedirectLogin()}
