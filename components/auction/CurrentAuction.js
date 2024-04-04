b61dd36e269e9c823b8f66984cefac48191af427
mert18 <mertuygur02@gmail.com>
2024-04-04 20:32:30 +0300

    6: Show user money on the screen

--- components/auction/CurrentAuction.js
+++ components/auction/CurrentAuction.js
@@ -5,12 +5,19 @@ import Countdown from "./Countdown";
 import { useSession } from "next-auth/react";
 import IncreaseBid from "./IncreaseBid";
 import AuctionItemHeader from "./AuctionItemHeader";
+import { getUserBalance } from "@/api/user";
+import UserBalance from "./UserBalance";
 
 const CurrentAuction = () => {
   const [auction, setAuction] = useState({});
   const [socket, setSocket] = useState(null);
   const [currentBid, setCurrentBid] = useState({ bid: 0, bidder: "" });
+  const [userBalance, setUserBalance] = useState(0);
   const { data: session } = useSession();
+
+  useEffect(() => {
+    console.log("User balance updated", userBalance);
+  }, [userBalance]);
 
   useEffect(() => {
     // Create a WebSocket connection
@@ -18,28 +25,35 @@ const CurrentAuction = () => {
     // WebSocket event handlers
     newSocket.addEventListener("open", (event) => {
       console.log("WebSocket connection opened", event);
+      getUserBalance(session?.user?.name).then((response) => {
+        console.log("User balance", response.data);
+      });
       setSocket(newSocket);
     });
 
     newSocket.addEventListener("message", (event) => {
       const incomingMessage = JSON.parse(event.data);
-      console.log("EVENT INCOMING!: ", incomingMessage)
 
-      if(incomingMessage.type === "auctionUpdate") {
-        console.log("setting auction: ", incomingMessage.data);
+      if (incomingMessage.type === "auctionUpdate") {
         setAuction(incomingMessage.data);
-        if(incomingMessage.data.currentBid || incomingMessage.data.currentBidder) {
+        if (
+          incomingMessage.data.currentBid ||
+          incomingMessage.data.currentBidder
+        ) {
           setCurrentBid({
             bid: incomingMessage.data.currentBid,
-            bidder: incomingMessage.data.currentBidder
+            bidder: incomingMessage.data.currentBidder,
           });
-        }else {
+        } else {
           setCurrentBid({
             bid: 0,
-            bidder: ""
+            bidder: "",
           });
         }
-      } else if(incomingMessage.type === "bidUpdate") {
+        getUserBalance(session?.user?.name).then((response) => {
+          setUserBalance(response.data);
+        });
+      } else if (incomingMessage.type === "bidUpdate") {
         setCurrentBid(incomingMessage.data);
       }
     });
@@ -65,7 +79,10 @@ const CurrentAuction = () => {
   };
 
   const handleIncreaseBid = (amount) => {
-    console.log("amount:", amount, "currentBidder:", session.user.name);
+    if(amount > userBalance.balance) {
+      console.error('Insufficient balance');
+      return;
+    }
     const message = {
       auctionId: auction.auctionId,
       itemId: auction.itemId,
@@ -76,6 +93,10 @@ const CurrentAuction = () => {
   };
 
   const handleSetBid = (amount) => {
+    if(amount > userBalance.balance) {
+      console.error('Insufficient balance');
+      return;
+    }
     const message = {
       auctionId: auction.auctionId,
       itemId: auction.itemId,
@@ -89,6 +110,7 @@ const CurrentAuction = () => {
     <div className="flex flex-col justify-center items-center w-full sm:w-2/3 md:w-1/3 border border-red-400 p-4">
       <AuctionItemHeader auction={auction} />
       <Countdown date={new Date(auction.auctionEnd)} />
+      <UserBalance userBalance={userBalance} />
       <CurrentBid currentBid={currentBid} />
       <IncreaseBid
         handleIncreaseBid={handleIncreaseBid}
