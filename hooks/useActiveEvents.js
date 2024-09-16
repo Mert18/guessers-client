93322a03a959bbe6c1e97e480d620b807a0e5f84
mert18 <mertuygur02@gmail.com>
2024-09-16 22:50:46 +0300

    77: events pagination and sort events

--- hooks/useActiveEvents.js
+++ hooks/useActiveEvents.js
@@ -13,8 +13,12 @@ export const useActiveEvents = (roomId) => {
       setLoading(true);
       try {
         const response = await getActiveEvents(roomId, paging);
-        console.log("response:", response)
         setActiveEvents(response?.data?.content);
+        setPaging({
+          page: response?.data?.page.number,
+          size: response?.data?.page.size,
+          totalPages: response?.data?.page.totalPages,
+        });
       } catch (error) {
         console.error("Error fetching active events", error);
       } finally {
