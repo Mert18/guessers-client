69e59e933a496a4fa94275aa6cbaf535f7d91b6a
mert18 <mertuygur02@gmail.com>
2024-12-07 20:37:44 +0300

    logout ts

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,4 +1,27 @@
-export default (req, res) => {
-  const url = process.env.NEXTAUTH_URL ?? "";
-  res.redirect(url);
-};
+import { getServerSession } from "next-auth";
+import { getIdToken } from "../../../../util/sessionTokenAccessor";
+import { authOptions } from "../utils/authOptions";
+import axios from "axios";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    const idToken = await getIdToken();
+
+    // this will log out the user on Keycloak side
+    const url = `${
+      process.env.END_SESSION_URL
+    }?id_token_hint=${idToken}&post_logout_redirect_uri=${encodeURIComponent(
+      process.env.NEXTAUTH_URL ?? ""
+    )}`;
+
+    try {
+      await axios.get(url);
+    } catch (err) {
+      console.error("error session logging out", err);
+      return new Response(null, { status: 500 });
+    }
+  }
+  return new Response(null, { status: 200 });
+}
