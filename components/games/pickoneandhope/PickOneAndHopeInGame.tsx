74b02648add8fd5480ea104db2149451a408a076
mert18 <mertuygur02@gmail.com>
2025-06-08 10:46:48 +0300

    :bee: pick one and hope game visual improvements

--- components/games/pickoneandhope/PickOneAndHopeInGame.tsx
+++ components/games/pickoneandhope/PickOneAndHopeInGame.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Client } from "@stomp/stompjs";
 import { IGameRoom } from "@/types/IPickOneAndHope";
 import Image from "next/image";
 import { GameStateEnum } from "@/enum/enum";
 import CustomButton from "@/components/common/CustomButton";
+import Loader from "@/components/common/Loader";
 
 interface InGameProps {
   client: Client;
@@ -12,29 +13,51 @@ interface InGameProps {
 }
 
 const PickOneAndHopeInGame = ({ client, roomInfo, onClose }: InGameProps) => {
-  const [gameStatusMessage, setGameStatusMessage] = React.useState<string>("");
-  const [round, setRound] = React.useState<number>(1);
-  const [roundPicks, setRoundPicks] = React.useState<string[]>([]);
-  const [gameStatus, setGameStatus] = React.useState<GameStateEnum>();
-  const [playersScore, setPlayersScore] = React.useState<{
+  const [gameStatusMessage, setGameStatusMessage] = useState<string>("");
+  const [round, setRound] = useState<number>(1);
+  const [roundPicks, setRoundPicks] = useState<string[]>([]);
+  const [gameStatus, setGameStatus] = useState<GameStateEnum>();
+  const [playersScore, setPlayersScore] = useState<{
     [key: string]: number;
   }>({
     [roomInfo.players[0].username]: 0,
     [roomInfo.players[1].username]: 0,
   });
+  const [matchResults, setMatchResults] = useState<
+    ("null" | "left" | "right")[]
+  >([]);
+  const [scoreDelta, setScoreDelta] = useState<{ [key: string]: number }>({});
+  const [triggerPulse, setTriggerPulse] = useState(false);
+  const [triggerMove, setTriggerMove] = useState(false);
 
   useEffect(() => {
     const topic = `/topic/room/${roomInfo.id}`;
 
     const subscription = client.subscribe(topic, (message) => {
       const data = JSON.parse(message.body);
-      console.log("Game message:", data);
       if (data.action === "START") {
         setGameStatusMessage("Starting the game...");
       } else if (data.action === "ROUND") {
         setGameStatus(GameStateEnum.IN_GAME);
         setRound(data.round);
         setRoundPicks(data.roundPicks);
+        setMatchResults(data.matchResults);
+        setTriggerPulse(true);
+        setTriggerMove(false);
+
+        setTimeout(() => {
+          setTriggerPulse(false);
+          setTriggerMove(true);
+        }, 1000);
+        setScoreDelta({
+          [roomInfo.players[0].username]: data.matchResults.filter(
+            (m: string) => m === "left"
+          ).length,
+          [roomInfo.players[1].username]: data.matchResults.filter(
+            (m: string) => m === "right"
+          ).length,
+        });
+        setTimeout(() => setScoreDelta({}), 1000); // hide after animation
         setPlayersScore({
           [roomInfo.players[0].username]:
             data.playersScore[roomInfo.players[0].username],
@@ -72,6 +95,12 @@ const PickOneAndHopeInGame = ({ client, roomInfo, onClose }: InGameProps) => {
         {objectRenderer(roomInfo.players[0].object)}
 
         {playersScore[roomInfo.players[0].username]}
+
+        {scoreDelta[roomInfo.players[0].username] ? (
+          <span className="text-green-500 animate-fade-in-up">
+            +{scoreDelta[roomInfo.players[0].username]}
+          </span>
+        ) : null}
       </div>
       <div className="flex-1">
         {gameStatus === GameStateEnum.GAME_ENDED ? (
@@ -109,23 +138,46 @@ const PickOneAndHopeInGame = ({ client, roomInfo, onClose }: InGameProps) => {
           </div>
         ) : (
           <div className="text-center mb-4">
-            <h2 className="text-xl font-bold">Round {round}</h2>
-            <p>{gameStatusMessage}</p>
-            <div className="flex justify-center items-center mt-4">
-              <div className="grid grid-cols-3 gap-4 mt-4">
-                {roundPicks.map((pick, index) => (
-                  <div className="w-14 h-14 flex items-center justify-center">
-                    <Image
-                      key={index}
-                      src={`/icons/games/pickoneandhope/${pick}.svg`}
-                      alt={pick}
-                      width={40}
-                      height={40}
-                    />
+            {roundPicks.length === 0 ? (
+              <>
+                <p>Loading the game...</p>
+                <Loader />
+              </>
+            ) : (
+              <>
+                <h2 className="text-xl font-bold">Round {round}</h2>
+                <p>{gameStatusMessage}</p>
+                <div className="flex justify-center items-center mt-4">
+                  <div className="grid grid-cols-3 gap-4 mt-4">
+                    {roundPicks.map((pick, index) => {
+                      const match = matchResults[index];
+                      const shouldPulse = triggerPulse && match !== "null";
+                      const shouldMove = triggerMove && match !== "null";
+
+                      return (
+                        <div
+                          key={index}
+                          className={`
+        w-14 h-14 flex items-center justify-center
+        transition-transform duration-500
+        ${shouldPulse ? "animate-pulse-grow" : ""}
+        ${shouldMove && match === "left" ? "animate-to-left" : ""}
+        ${shouldMove && match === "right" ? "animate-to-right" : ""}
+      `}
+                        >
+                          <Image
+                            src={`/icons/games/pickoneandhope/${pick}.svg`}
+                            alt={pick}
+                            width={40}
+                            height={40}
+                          />
+                        </div>
+                      );
+                    })}
                   </div>
-                ))}
-              </div>
-            </div>
+                </div>
+              </>
+            )}
           </div>
         )}
       </div>
@@ -133,6 +185,11 @@ const PickOneAndHopeInGame = ({ client, roomInfo, onClose }: InGameProps) => {
         {roomInfo.players[1].username}
         {objectRenderer(roomInfo.players[1].object)}
         {playersScore[roomInfo.players[1].username]}
+        {scoreDelta[roomInfo.players[1].username] ? (
+          <span className="text-green-500 animate-fade-in-up">
+            +{scoreDelta[roomInfo.players[1].username]}
+          </span>
+        ) : null}
       </div>
     </div>
   );
